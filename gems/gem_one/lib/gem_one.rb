f91ba5a3a2a9a35656fb640bf3812a5fd5a61f60
Joe Letizia <joe.letizia@viewthespace.com>
2021-04-08 13:47:28 -0400

    Add internal inheritance to show lack of problem for internally defined classes with inheritance

--- gems/gem_one/lib/gem_one.rb
+++ gems/gem_one/lib/gem_one.rb
@@ -8,4 +8,24 @@ module GemOne
     x.foo
   end
 
+  sig{params(x: MyChild).returns(String)}
+  def boo(x)
+    x.internal_foo
+  end
+
+  class MyParent
+    extend T::Sig
+    sig{returns(String)}
+    def internal_foo
+      "internal_foo"
+    end
+  end
+
+  class MyChild < MyParent
+    extend T::Sig
+    sig{returns(String)}
+    def internal_bar
+      "internal_bar"
+    end
+  end
 end
